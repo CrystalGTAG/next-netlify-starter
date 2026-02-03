2c19e0678518ac95d581d93a3c3c5e0758aed3c0
CrystalGTAG <damonkeyiscool6000@gmail.com>
2026-02-03 05:42:16 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Averxity.lol!</title>
         <link rel="icon" href="/favicon.ico"/>
       </Head>
 
       <main>
-        <Header title="Welcome to Averxity!"/>
+        <Header title="Welcome to Averxity.lol!"/>
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
         </p>
       </main>
 
